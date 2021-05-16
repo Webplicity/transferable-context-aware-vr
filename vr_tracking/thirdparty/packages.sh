3c544dbcdefdc8fd195793527af97980e51f55f6
CharlesDerek <charlesderek177@gmail.com>
2021-05-16 11:10:23 -0400

    install scripts

---
+++ vr_tracking/thirdparty/packages.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install -y libglew-dev
+sudo apt-get install -y qt5-default
+sudo apt-get install -y libssl-dev
+sudo apt install libsdl2-dev libsdl2-2.0-0 -y
+sudo apt install libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 -y
+sudo apt install libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 -y
+sudo apt install libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0 -y
